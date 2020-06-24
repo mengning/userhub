0fd81c219c86289424996a012b91ee21f7b97a76
mengning <mengning@ustc.edu.cn>
2020-06-25 02:02:36 +0800

    get arcode ID in scan event

--- wechatscan.js
+++ wechatscan.js
@@ -3,8 +3,19 @@ const wechat = require('wechat');
 const wechatscan = {};
 wechatscan.message = async function (req, res, next) {
     var message = req.weixin;
+    var scanCode = '';
     console.log(message);
-    res.reply({content:'wellcome!', type: 'text' });
+    if (message.Event === 'subscribe') {
+        var key = message.EventKey
+        if (key.indexOf("qrscene_") !== -1) {
+            var start = key.indexOf('qrscene_') + ('qrscene_'.length);
+            scanCode = key.substring(start);
+        }
+    }
+    if (message.MsgType === 'event' && message.Event === 'SCAN') {
+        scanCode = message.EventKey;
+    }
+    res.reply({ content: 'wellcome! ' + scanCode, type: 'text' });
 }
 wechatscan.start = function (app) {
     app.use('/wechat', wechat('wechat', wechatscan.message));
